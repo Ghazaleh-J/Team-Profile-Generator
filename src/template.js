c20155832ed91a44807e0a22de1f435ff5683f3c
Ghazaeh-J <ghazal.javadi@gmail.com>
2022-05-31 12:33:33 -0700

    Added  styling

--- src/template.js
+++ src/template.js
@@ -5,14 +5,14 @@ function manager(employeeArray) {
     })
     .map((managerCard) => {
       return `<div class="card" style="width: 18rem;">
-        <div class="card-body">
+        <div class="card-body header">
           <h5 class="card-title">${managerCard.name}</h5>
           <p class="card-text">${managerCard.getRole()}</p>
         </div>
         <div class="card-body">
         <ul class="list-group list-group-flush">
         <li class="list-group-item">Id: ${managerCard.id}</li>
-        <li class="list-group-item">Email: ${managerCard.email}</li>
+        <li class="list-group-item">Email: <a href="mailto:${managerCard.email}">${managerCard.email}</a></li>
         <li class="list-group-item">Office Number: ${managerCard.officeNumber}</li>
       </ul>
         </div>
@@ -27,7 +27,19 @@ function engineer(employeeArray) {
       return employee.getRole() === "Engineer";
     })
     .map((engineerCard) => {
-      return `<div> ${engineerCard.name}</div>`;
+      return `<div class="card" style="width: 18rem;">
+      <div class="card-body header">
+        <h5 class="card-title">${engineerCard.name}</h5>
+        <p class="card-text">${engineerCard.getRole()}</p>
+      </div>
+      <div class="card-body">
+      <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id: ${engineerCard.id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${engineerCard.email}">${engineerCard.email}</a></li>
+      <li class="list-group-item">Github Username: <a href="https://github.com/${engineerCard.github}" target="_blank">${engineerCard.github}</a></li>
+    </ul>
+      </div>
+    </div>`;
     })
     .join("");
 }
@@ -38,7 +50,19 @@ function intern(employeeArray) {
       return employee.getRole() === "Intern";
     })
     .map((internCard) => {
-      return `<div> ${internCard.name}</div>`;
+      return `<div class="card" style="width: 18rem;">
+      <div class="card-body header">
+        <h5 class="card-title">${internCard.name}</h5>
+        <p class="card-text">${internCard.getRole()}</p>
+      </div>
+      <div class="card-body">
+      <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id: ${internCard.id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${internCard.email}">${internCard.email}</a></li>
+      <li class="list-group-item">School: ${internCard.school}</li>
+    </ul>
+      </div>
+    </div>`;
     })
     .join("");
 }
@@ -51,13 +75,20 @@ function generateTeam(employeeArray) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="./Assets/style.css">
     <title>Team Generator</title>
 </head>
 <body>
+
+<header>
+<h1> My Team </h1>
+</header>
+
+<main class="cardContainer">
 ${manager(employeeArray)}
 ${engineer(employeeArray)}
 ${intern(employeeArray)}
+</main>
     
 </body>
 </html>`; // html body, head "entire doc"
